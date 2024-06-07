b1b4976242cfa3972f97c087b1d722a75d54b047
mayowa <mtudonu85@gmail.com>
2024-06-07 15:48:03 +0100

    fix broken ci-cd pipeline

--- test/DoefinV1OrderBook.t.sol
+++ test/DoefinV1OrderBook.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.19;
 
-import "forge-std/Console.sol";
 import { Base_Test } from "./Base.t.sol";
 import { Test } from "forge-std/Test.sol";
 import { DoefinV1OrderBook } from "../src/DoefinV1OrderBook.sol";
