776b5e0ad6cf489c924a4ba1b38d73ee16f98b40
mayowa <mtudonu85@gmail.com>
2024-10-07 07:36:23 +0100

    modify deployment script for block header oracle and mock token

---
+++ script/DeployMockToken.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.19 <0.9.0;
+
+import { BaseScript } from "./Base.s.sol";
+import { DoefinV1Config } from "../src/DoefinV1Config.sol";
+import { MockToken } from "../test/mocks/MockToken.sol";
+
+/// @notice Deploys MockToken
+contract DeployMockToken is BaseScript {
+    address public feeAddress;
+    uint256 public initialBlockHeight;
+
+    function run(address configAddress) public virtual returns (MockToken mockToken) {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        mockToken = MockToken(0x1CE730de98ff5a144980D57dbfcc8d5011058aD7);
+
+        DoefinV1Config(configAddress).addTokenToApprovedList(address(0x1CE730de98ff5a144980D57dbfcc8d5011058aD7), 100);
+
+        vm.stopBroadcast();
+    }
+}
